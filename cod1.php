83f03564e1c932a7eebb6315fff53d8c44ed231a
imdoarda <dudinha.mgalvao@gmail.com>
2024-03-13 12:16:05 -0300

    criação cód p/ dizer se é negativo, positivo ou neutro

---
+++ cod1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tipo número</title>
+</head>
+<body>
+    <h1>Par ou Ímpar?</h1>
+    <form method="post">
+     <label for="num">número:</label>
+     <input type="number" step="0.01" name="num" />
+     <input type="submit" name="submit" value="Calcular" />
+    </form>
+ 
+    <?php
+    $num = $_POST["num"];
+ 
+    if($num % 2 == 0){
+        echo "O número informado é par.";
+    }else{
+        echo "O número informado é ímpar.";
+ }
+    ?>
+</body>
+</html>
---
+++ cod2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tipo número</title>
+</head>
+<body>
+    <h1>Redondo ou não?</h1>
+    <form method="post">
+     <label for="num">número:</label>
+     <input type="number" step="0.01" name="num" />
+     <input type="submit" name="submit" value="Calcular" />
+    </form>
+ 
+    <?php
+     $num = $_POST["num"];
+ 
+    if ($num % 10 == 0)
+        {
+            echo "O número informado é redondo";
+        } else
+        {
+            echo "O número informado não é redondo";
+        }
+    ?>
+</body>
+</html>
