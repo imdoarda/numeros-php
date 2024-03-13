2bc207e066295a4b81aa43dad2cf37be21a86122
imdoarda <dudinha.mgalvao@gmail.com>
2024-03-13 09:32:11 -0300

    atividades em php

---
+++ cod3.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tipo número</title>
+</head>
+<body>
+    <h1>Positivo, Negativo ou Neutro?</h1>
+    <form method="post">
+     <label for="num">número:</label>
+     <input type="number" step="0.01" name="num" />
+     <input type="submit" name="submit" value="Calcular" />
+    </form>
+ 
+    <?php
+     $num = $_POST["num"];
+ 
+    if ($num > 0)
+        {
+            echo "O número informado é positivo";
+        }elseif($num < 0)
+        {
+            echo "O número informado é negativo";
+        }else{
+            echo "O número informado é neutro";
+        }
+
+    ?>
+</body>
+</html>
